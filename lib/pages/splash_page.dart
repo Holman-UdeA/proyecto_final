a5a598d6c06dbd36fc11de0958e89f558e569947
holman.londono <holman.londono@udea.edu.co>
2025-06-24 20:41:43 -0500

    Se termina la implementación del Splash Page, donde se valida si hay una sesión iniciada o si se debe dirigir a la página de inicio de sesión.

--- lib/pages/splash_page.dart
+++ lib/pages/splash_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:proyecto_final/pages/home_navigation_bar_page.dart';
+import 'package:proyecto_final/repository/firebase_api.dart';
 
 class SplashPage extends StatefulWidget {
   const SplashPage({super.key});
@@ -8,8 +10,41 @@ class SplashPage extends StatefulWidget {
 }
 
 class _SplashPageState extends State<SplashPage> {
+  final FirebaseApi _firebaseApi = FirebaseApi();
+
+  @override
+  void initState() {
+    _closeSplash();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Scaffold(
+      body: Center(
+        child: Image(
+          image: AssetImage("assets/images/logo.png"),
+          height: 200,
+          width: 200,
+        ),
+      ),
+    );
+  }
+
+  void _closeSplash() {
+    Future.delayed(const Duration(seconds: 2), () async {
+      var result = _firebaseApi.validateSession();
+      if (await result) {
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => SplashPage()),
+        );
+      }else{
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => HomeNavigationBarPage()),
+        );
+      }
+    });
   }
 }
